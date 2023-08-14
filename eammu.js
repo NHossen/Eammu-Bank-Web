4c9ec4e3bd4073698795882472069c5119ee22e2
NHossen <naeemhossen2509@gmail.com>
2023-08-14 21:52:57 +0400

    email and password verification added by js

---
+++ eammu.js
@@ -0,0 +1,29 @@
+// Button js
+//step 1 add eventlisteners
+document.getElementById('btn-submit').addEventListener("click",function(){
+    //console.log('btn-submited');
+
+    //step 2 get the get email address
+    //*** always remember to use .value to get text from an input field */
+    const emailField=document.getElementById('user-email');
+    //const getEmail=emailField.value;
+    const getEmail=emailField.value;
+    //console.log(getEmail);
+    
+    //step 3 get password
+    const passwordField=document.getElementById('user-password');
+    const getPassword=passwordField.value;
+    //console.log(getPassword);
+
+//danger:::: Do not verify email password on the client side
+//step:4 verify password
+
+ if (getEmail === "naeemhossen2509@gmail.com" && getPassword ==='naeem'){
+    alert('Welcome to Eammu Bank')
+ }else{
+    alert('Please Provide Valid email and password')
+}
+    
+ 
+
+})
