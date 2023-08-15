03b7e8d273cb6a4e6ef41c52640b756df48cedd4
NHossen <naeemhossen2509@gmail.com>
2023-08-15 12:37:02 +0400

    javascript file updated

---
+++ script.js
@@ -0,0 +1,54 @@
+//Deposit section
+document.getElementById('btn-deposit').addEventListener('click',function(){//step number 1
+    //console.log('I got clicked');
+
+    //2 step get the deposit ammount deposit-field
+
+    const depositAmmountMain=document.getElementById('deposit-field');
+    const depositTotalMain=depositAmmountMain.value;
+//Convert value string to number
+    const stringToNumDeposit=parseFloat(depositTotalMain)
+
+      //step-7 - extra clear the deposit field
+
+      depositAmmountMain.value="";
+
+    if(isNaN(stringToNumDeposit)){
+        alert("Please enter your Deposit amount")
+        return;
+    }
+
+    //console.log(depositTotalMain);
+
+    // step:3 get the deposit ammount
+
+   //for non input field need to use innerText to get the text
+    const depositTotalElement=document.getElementById('deposit-total');
+    const prevDepositTotal=depositTotalElement.innerText;
+    // conver value string to number
+    const stringToNumPrevTotal=parseFloat(prevDepositTotal)
+
+// step 4 Add numbers to set the total deposit 
+    const depositSum=stringToNumPrevTotal + stringToNumDeposit;
+
+    //set the deposit total
+    depositTotalElement.innerText=depositSum
+
+    //console.log(depositTotal);
+
+// step - 5 get ballance current total
+const balanceTotalElement=document.getElementById("balance-total");
+const prevBalanceTotalString=balanceTotalElement.innerText;
+const prevBalanceTotal=parseFloat(prevBalanceTotalString);
+
+// step 6 calculate current totAL BALA
+const currrenTotalBalance=prevBalanceTotal + stringToNumDeposit;
+
+// Set the current balance
+balanceTotalElement.innerText=currrenTotalBalance;
+
+
+
+  
+
+})
